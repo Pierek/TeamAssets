739e7766e4066a1bf47c20caab1dd7d31dedeb92
Piotr Purwin <purwinp@gmail.com>
2017-11-07 15:37:24 +0100

    1

--- TeamExport/TeamExport/data/Tables/price.sql
+++ TeamExport/TeamExport/data/Tables/price.sql
@@ -3,6 +3,8 @@
 	 [product_id]      int NOT NULL
 	,[price_client_id] int NOT NULL
 	,[price_value]     int NOT NULL
+	,[price_currency]  varchar(10) NOT NULL
 	,[LastUpdate]      datetime NOT NULL
 	,[LastUser]        varchar(100) NOT NULL
+	,CONSTRAINT [PK_price] PRIMARY KEY CLUSTERED(product_id, price_client_id, price_value)
 );
