d4aba619998191cc93874ba5a60cb218a73b167f
Piotrek <purwinp@gmail.com>
2018-02-06 19:29:26 +0100

    abc

---
+++ TeamExport/TeamExport/log/Tables/job_log.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [log].[job_log]
+(
+	 [job_id]          int NOT NULL IDENTITY(1,1) PRIMARY KEY
+	,[start_datetime]  datetime NULL
+	,[finish_datetime] datetime NULL
+	,[status]          varchar(10) NULL
+)
