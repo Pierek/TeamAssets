c8fa9f05bf93f8e0acb8a3d9c5742e1d6d41a94a
Slawek <s.e.pietrzyk@gmail.com>
2018-04-23 12:51:15 +0200

    added production config

--- project/service/config.py
+++ project/service/config.py
@@ -22,3 +22,16 @@ class DevelopmentSlawekConfig:
     TEAM_USER = 'teampolska'
     TEAM_PWD = 'teampolska'
 
+class ProductionConfig:
+    """Development slawek configuration."""
+    DEBUG = True
+    API_USERNAME = 'a@pi.com'
+    API_PWD = 'api123'
+    URL = 'http://127.0.0.1:5000/'
+    TEAM_SERVER = 'localhost\MSSQLSERVER01'
+    TEAM_DATABASE = 'TeamExport'
+    TEAM_USER = 'teampolska'
+    TEAM_PWD = 'teampolska'
+
+    
+
