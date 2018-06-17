4bd482e0d1c5dfaf01c8c1b8d445577608fc2a04
Piotrek <purwinp@gmail.com>
2018-06-18 00:26:29 +0200

    add if success then run

--- project/entity/model.py
+++ project/entity/model.py
@@ -2,37 +2,44 @@ import service.qry as qry
 from api.request import api_request
 import logging
 
-def job_log(token):
-    """send job_log data request from database to the server"""      
-     
+class Job:
+
     # retrieve all data from the table
-    log_row = qry.Cursor().queryresult(
-                                        """ SELECT TOP 1
-                                                 job_id
-                                                ,CONVERT(varchar(24),start_datetime,120)
-                                                ,CONVERT(varchar(24),finish_datetime,120)
-                                                ,status
-                                            FROM log.job_log ORDER BY job_id DESC """)
+    def __init__(self, token):
+        self.log_row = qry.Cursor().queryresult(
+                                            """ SELECT TOP 1
+                                                     job_id
+                                                    ,CONVERT(varchar(24),start_datetime,120)
+                                                    ,CONVERT(varchar(24),finish_datetime,120)
+                                                    ,status
+                                                FROM log.job_log ORDER BY job_id DESC """)
+        self.token = token
 
-    items = {}
-    list_of_items = []
-    each_item = {}
+    def job_log(self):
+        """send job_log data request from database to the server"""      
+     
+        items = {}
+        list_of_items = []
+        each_item = {}
 
-    # create json-like dictionary
-    if log_row:  # check if list is not empty
-        each_item['id'] = log_row[0][0]
-        each_item['start_datetime'] = log_row[0][1]
-        each_item['finish_datetime'] = log_row[0][2]
-        each_item['status'] = log_row[0][3]
-        list_of_items.append(each_item.copy())
+        # create json-like dictionary
+        if self.log_row:  # check if list is not empty
+            each_item['id'] = self.log_row[0][0]
+            each_item['start_datetime'] = self.log_row[0][1]
+            each_item['finish_datetime'] = self.log_row[0][2]
+            each_item['status'] = self.log_row[0][3]
+            list_of_items.append(each_item.copy())
 
-        # items is a final dictionary with a chunk data
-        items['items'] = list_of_items
+            # items is a final dictionary with a chunk data
+            items['items'] = list_of_items
 
-        # send request
-        api_request(token=token, jsondata=items, action='post', api_entity='api/job/')
+            # send request
+            api_request(token=self.token, jsondata=items, action='post', api_entity='api/job/')
 
-    logging.info('Token refreshed')
+        logging.info('Token refreshed')
+
+    def status(self):
+        return str(self.log_row[0][3])
 
 
 def product(token, action):
--- project/fullrefresh.py
+++ project/fullrefresh.py
@@ -1,7 +1,7 @@
 # project/fullrefresh.py
 
 from api.request import token_refresh
-from entity.model import product, client_dict, price_client_dict, stock, price, job_log
+from entity.model import product, client_dict, price_client_dict, stock, price, Job
 import logging
 import os
 import datetime
@@ -15,33 +15,35 @@ logging.basicConfig(filename=LOG_LOCATION + 'fullrefresh' + datetime.datetime.to
 token = token_refresh()
 
 # run job_log request
-job_log(token=token)
+a = Job(token=token)
+
+a.job_log()
+
+if a.status() == 'Success':
+    # run product data
+    product(token=token, action='delete')
+    product(token=token, action='put')
+    product(token=token, action='post')
 
 
-# run product data
-product(token=token, action='delete')
-product(token=token, action='put')
-product(token=token, action='post')
+    ## run client data
+    client_dict(token=token, action='delete')
+    client_dict(token=token, action='put')
+    client_dict(token=token, action='post')
 
 
-## run client data
-client_dict(token=token, action='delete')
-client_dict(token=token, action='put')
-client_dict(token=token, action='post')
+    ## run price_client data
+    price_client_dict(token=token, action='delete')
+    price_client_dict(token=token, action='put')
+    price_client_dict(token=token, action='post')
 
 
-## run price_client data
-price_client_dict(token=token, action='delete')
-price_client_dict(token=token, action='put')
-price_client_dict(token=token, action='post')
+    # run stock data
+    stock(token=token, action='put')
+    stock(token=token, action='post')
 
 
-# run stock data
-stock(token=token, action='put')
-stock(token=token, action='post')
-
-
-# run price data
-price(token=token, action='put')
-price(token=token, action='post')
+    # run price data
+    price(token=token, action='put')
+    price(token=token, action='post')
 
